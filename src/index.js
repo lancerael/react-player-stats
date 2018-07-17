ca8938d89de11e5044f66817175a9aa9dec3cf22
lancerael <lancerael@hotmail.com>
2018-07-17 17:00:33 +0100

    Removed unused state reference.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlayerStats from './components/PlayerStats';
-// import store from '../state';
 import './../styles/index.scss';
 
 ReactDOM.render(<PlayerStats />, document.getElementById('rps'));
